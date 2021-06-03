8052dc47370aa5af1b5724fa99c61971701e2006
lsh <rrgks@naver.com>
2021-06-03 22:29:38 +0900

    그리디 알고리즘 문제풀이

---
+++ 그리디/전자레인지.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().split(" ");
+
+const second = parseInt(input[0]);
+
+function minBtn (second) {
+    const btnTime = [300, 60, 10];
+    const btnCount = [];
+    
+    if (!((second % btnTime[2]) === 0)) return -1;
+
+    for (let i of btnTime) {
+        btnCount.push(parseInt(second / i));
+        second %= i;
+    }
+    return btnCount.join(" ");
+}
+
+console.log(minBtn(second));
