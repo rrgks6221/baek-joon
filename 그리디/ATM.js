0bf5e1489de1b8d5cbd6b8e27b2218877aa10d2a
lsh <rrgks@naver.com>
2021-06-04 15:13:24 +0900

    ATM 풀이

---
+++ 그리디/ATM.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+const arr = input[1].split(" ");
+
+function solution(arr) {
+    let temp = 0;
+    let result = 0;
+    arr.sort((a, b) => {
+        return a - b;
+    })
+
+    for (let i of arr) {
+        temp += parseInt(i);
+        result += temp;
+    }
+    return result;
+}
+
+console.log(solution(arr));
