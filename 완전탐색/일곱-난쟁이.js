cfb8d82ba1dd4f8cc9d19e8c3207b2dd226f37d7
lsh <rrgks@naver.com>
2021-07-18 23:49:05 +0900

    일곱 난쟁이 풀이

---
+++ 완전탐색/일곱-난쟁이.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n").map(v => +v);
+
+const over = input.reduce((acc, cur) => acc += cur, 0) - 100;
+
+for (let i = 0; i < input.length - 1; i += 1) {
+    for (let j = i + 1; j < input.length; j += 1) {
+        if (over === input[i] + input[j]) {
+            [input[i], input[j]] = [0, 0];
+            input.sort((a, b) => a - b).splice(0, 2);
+            break;
+        }
+    }
+    if (input.length === 7) break;
+}
+
+for (let i of input) console.log(i);
