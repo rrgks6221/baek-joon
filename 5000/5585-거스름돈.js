f67c235e55f5aba74b3295110a492f9e21d7b94b
lsh <rrgks@naver.com>
2022-02-04 21:31:40 +0900

    5585-거스름돈 풀이

---
+++ 5000/5585-거스름돈.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split(' ');
+
+const cost = input[0];
+
+function changeCount(cost) {
+  const moneykinds = [500, 100, 50, 10, 5, 1];
+  let changeMoney = 1000 - cost;
+  let count = 0;
+
+  for (let i of moneykinds) {
+    while (changeMoney >= i) {
+      count += parseInt(changeMoney / i);
+      changeMoney %= i;
+    }
+  }
+  return count;
+}
+
+console.log(changeCount(cost));
